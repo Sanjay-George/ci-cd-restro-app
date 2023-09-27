45600451e720090e01dedc07e65af2938e0f6337
sgeorge-nb <122819133+sgeorge-nb@users.noreply.github.com>
2023-09-27 09:46:31 +0530

    Refactor playbooks
    - Create site.yml as main playbook
    - Use group patterns to limit execution in deploy job

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,8 +4,9 @@ echo $ANSIBLE_VAULT_PASSWORD > .vault
 
 echo "playbook: $1"
 echo "inventory: $2"
+echo "limit: $3"
 
 ## TODO: TAKE INVENTORY AND PLAYBOOK AS INPUT
-ansible-playbook -i $2 $1 --vault-password-file .vault
+ansible-playbook -i $2 -l $3 $1 --vault-password-file .vault
 
 rm .vault
